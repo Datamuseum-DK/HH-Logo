b43a4589a20e5241f68f35d7b5a70138f64c0710
Poul-Henning Kamp <phk@FreeBSD.org>
2023-02-04 11:55:07 +0000

    Add warning-yellow-background for test-env

--- ddhf_hh_logo_cairo.py
+++ ddhf_hh_logo_cairo.py
@@ -4,6 +4,9 @@ import cairo
 
 from ddhf_hh_logo import DDHF_HH_Logo
 
+TA_COLOR=(0, .3, 1)
+TEST_COLOR=(0xff/0xff, 0xd7/0xff, 0x00/0xff)
+
 def Logo_Cairo(ctx, txt=None, txtsz=None, txtcol=None):
     if txt:
         ctx.save()
@@ -22,33 +25,49 @@ def Logo_Cairo(ctx, txt=None, txtsz=None, txtcol=None):
     ctx.stroke()
 
 def Logo_TA_Cairo(ctx):
-    Logo_Cairo(ctx, txt="TA", txtsz=700, txtcol=(0,.3,1))
+    Logo_Cairo(ctx, txt="TA", txtsz=700, txtcol=TA_COLOR)
     
 
 if __name__ == "__main__":
     # The basic logos
-    for fn, func,size in (
-         ("hh_logo_100.svg", Logo_Cairo, 100),
-         ("hh_logo_1000.svg", Logo_Cairo, 1000),
-         ("hh_logo_ta_100.svg", Logo_TA_Cairo, 100),
-         ("hh_logo_ta_1000.svg", Logo_TA_Cairo, 1000),
-    ):
-        surface = cairo.SVGSurface(fn, size, size)
-        ctx = cairo.Context(surface)
-        ctx.scale(size/1000, size/1000)
-        func(ctx)
+    for test in (True, False):
+        for fn, func,size in (
+             ("hh_logo_100", Logo_Cairo, 100),
+             ("hh_logo_ta_100", Logo_TA_Cairo, 100),
+        ):
+            if test:
+                fn += "_test"
+            fn += ".svg"
+ 
+            surface = cairo.SVGSurface(fn, size, size)
+            ctx = cairo.Context(surface)
+            if test:
+                ctx.save()
+                ctx.set_source_rgb(*TEST_COLOR)
+                ctx.paint()
+                ctx.fill()
+                ctx.restore()
+            ctx.scale(size/1000, size/1000)
+            func(ctx)
 
-    # Make a favicon with light gray background
-    for fn, func,size in (
-         ("favicon.svg", Logo_Cairo, 100),
-         ("favicon_ta.svg", Logo_TA_Cairo, 100),
-    ):
-        surface = cairo.SVGSurface(fn, size, size)
-        ctx = cairo.Context(surface)
-        ctx.save()
-        ctx.set_source_rgb(0.9, 0.9, 0.9)
-        ctx.paint()
-        ctx.fill()
-        ctx.restore()
-        ctx.scale(size/1000, size/1000)
-        func(ctx)
+        # Make a favicon with light gray background
+        for fn, func,size in (
+             ("favicon", Logo_Cairo, 100),
+             ("favicon_ta", Logo_TA_Cairo, 100),
+        ):
+            if test:
+                fn += "_test"
+            fn += ".svg"
+ 
+            surface = cairo.SVGSurface(fn, size, size)
+            ctx = cairo.Context(surface)
+            ctx.save()
+            if test:
+                ctx.set_source_rgb(*TEST_COLOR)
+            else:
+                ctx.set_source_rgb(0.9, 0.9, 0.9)
+            ctx.paint()
+            ctx.fill()
+            ctx.restore()
+            ctx.scale(size/1000, size/1000)
+            func(ctx)
